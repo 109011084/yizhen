5c3ddc53de36ca5b136dc5efba74644f5c44b33d
109011084 <s103390@gmail.com>
2022-05-31 19:43:04 +0800

    fifth

--- app.py
+++ app.py
@@ -42,31 +42,31 @@ def handle_message(event):
 
     if msg == "hi":
         r = "hi"
-    elif msg in "你在幹嘛":
+    elif msg in "你在幹嘛呀唷":
         r = "我在想你呀~"
-    elif msg in "早安":
+    elif msg in "宜蓁早安安":
         r = "宇誠早安~宜蓁想你了"
-    elif msg in "晚安":
+    elif msg in "宜蓁晚安安":
         r = "晚安~愛你唷"
     elif msg in "我好想你":
         r = "我也很想你唷"
-    elif msg in "抱抱":
+    elif msg in "想要抱抱":
         r = "給你一個大抱抱~！"
-    elif msg in "嫁":
+    elif msg in "宜蓁願意嫁給我嗎":
         r = "好呀！那你要常常做家事了XD"
-    elif msg in "娶":
+    elif msg in "宜蓁願意讓我娶妳嗎":
         r = "Yes I do"
     elif msg in "宇誠很帥":
         r = "但宜蓁最可愛"
-    elif msg in "煩":
+    elif msg in "今天有點好煩哦":
         r = "什麼事 跟宜蓁說說"
-    elif msg in "在嗎":
+    elif msg in "宜蓁在嗎":
         r = "宜蓁在"
-    elif msg in "愛你":
+    elif msg in "愛你唷":
         r = "真的嗎 我也是耶 好剛好唷"
-    elif msg in "睡覺":
+    elif msg in "一起想睡覺覺了":
         r = "宜蓁也要一起~~ (躦"
-    elif msg in "無聊":
+    elif msg in "我宇誠好無聊":
         r = "那宜蓁陪你聊聊天"
     elif msg in "宇誠":
         r = "是宜蓁的"
